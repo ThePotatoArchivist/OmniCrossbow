dbbe417d8f8860526aef5fe9dd84af2348d613bb
JosiahFu <josiahfu@gmail.com>
2024-11-07 15:37:05 -0800

    supoprt entity buckets, other cleanup

--- src/main/java/archives/tater/omnicrossbow/OmniEnchantment.java
+++ src/main/java/archives/tater/omnicrossbow/OmniEnchantment.java
@@ -1,5 +1,7 @@
 package archives.tater.omnicrossbow;
 
+import archives.tater.omnicrossbow.mixin.BoatItemAccessor;
+import archives.tater.omnicrossbow.mixin.EntityBucketItemAccessor;
 import archives.tater.omnicrossbow.mixin.FallingBlockInvoker;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.enchantment.Enchantment;
@@ -9,7 +11,10 @@ import net.minecraft.entity.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.*;
 import net.minecraft.entity.projectile.thrown.*;
+import net.minecraft.entity.vehicle.BoatEntity;
+import net.minecraft.entity.vehicle.ChestBoatEntity;
 import net.minecraft.item.*;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
@@ -46,7 +51,14 @@ public class OmniEnchantment extends Enchantment {
         return constructor.create(world, shooter, velocity.x, velocity.y, velocity.z);
     }
 
-    public static @Nullable Entity createProjectile(World world, LivingEntity shooter, ItemStack projectile) {
+    private static @Nullable Entity create(ServerWorld world, EntityType<?> type, LivingEntity shooter, ItemStack projectile, SpawnReason spawnReason) {
+        var entity = type.create(world, projectile.getNbt(), null, shooter.getBlockPos(), spawnReason, false, false);
+        if (entity == null) return null;
+        entity.refreshPositionAndAngles(shooter.getX(), shooter.getEyeY() - 0.1f, shooter.getZ(), shooter.getYaw(), entity.getPitch());
+        return entity;
+    }
+
+    public static @Nullable Entity createProjectile(ServerWorld world, LivingEntity shooter, ItemStack projectile) {
         if (projectile.isOf(Items.EGG)) return new EggEntity(world, shooter);
         if (projectile.isOf(Items.ENDER_PEARL)) return new EnderPearlEntity(world, shooter);
         if (projectile.isOf(Items.SNOWBALL)) return new SnowballEntity(world, shooter);
@@ -64,20 +76,22 @@ public class OmniEnchantment extends Enchantment {
         if (projectile.isOf(Items.WITHER_SKELETON_SKULL)) return shootExplosive(world, shooter, 1f, WitherSkullEntity::new);
         if (projectile.isOf(Items.FIRE_CHARGE)) return shootExplosive(world, shooter, 1f, SmallFireballEntity::new);
         if (projectile.isOf(Items.DRAGON_BREATH)) return shootExplosive(world, shooter, 1f, DragonFireballEntity::new);
+        if (projectile.isOf(Items.ARMOR_STAND)) return create(world, EntityType.ARMOR_STAND, shooter, projectile, SpawnReason.SPAWN_EGG);
 
         var projectileItem = projectile.getItem();
-        if (projectileItem instanceof ThrowablePotionItem) {
-            return new PotionEntity(world, shooter);
-        }
+        if (projectileItem instanceof ThrowablePotionItem) return new PotionEntity(world, shooter);
         if (projectileItem instanceof BlockItem blockItem && blockItem.getBlock() instanceof FallingBlock fallingBlock) {
             var entity =  newFallingBlockEntity(world, shooter.getX(), shooter.getEyeY() - 0.1f, shooter.getZ(), fallingBlock.getDefaultState());
             ((FallingBlockInvoker) fallingBlock).invokeConfigureFallingBlockEntity(entity);
             return entity;
         }
-        if (projectileItem instanceof SpawnEggItem spawnEggItem) {
-            var entity = spawnEggItem.getEntityType(null).create((ServerWorld) world, projectile.getNbt(), null, shooter.getBlockPos(), SpawnReason.SPAWN_EGG, false, false);
-            if (entity == null) return null;
-            entity.refreshPositionAndAngles(shooter.getX(), shooter.getEyeY() - 0.1f, shooter.getZ(), shooter.headYaw, shooter.getPitch());
+        if (projectileItem instanceof EntityBucketItem entityBucketItem) return create(world, ((EntityBucketItemAccessor) entityBucketItem).getEntityType(), shooter, projectile, SpawnReason.BUCKET);
+        if (projectileItem instanceof SpawnEggItem spawnEggItem) return create(world, spawnEggItem.getEntityType(null), shooter, projectile, SpawnReason.SPAWN_EGG);
+        if (projectileItem instanceof BoatItem boatItem) {
+            var entity = ((BoatItemAccessor) boatItem).getChest()
+                    ? new ChestBoatEntity(world, shooter.getX(), shooter.getEyeY() - 0.1f, shooter.getZ())
+                    : new BoatEntity(world, shooter.getX(), shooter.getEyeY() - 0.1f, shooter.getZ());
+            entity.setVariant(((BoatItemAccessor) boatItem).getType());
             return entity;
         }
         if (projectileItem instanceof ArrowItem || projectile.isOf(Items.FIREWORK_ROCKET)) {
@@ -86,10 +100,11 @@ public class OmniEnchantment extends Enchantment {
         return null; // TODO: Generic projectile item
     }
 
-    public static void setupProjectile(Entity entity, World world, LivingEntity shooter, ItemStack projectile) {
+    public static void setupProjectile(Entity entity, LivingEntity shooter, ItemStack projectile) {
         if (entity instanceof EyeOfEnderEntity) return;
 
         if (entity instanceof ExplosiveProjectileEntity) {
+            // velocity already handled by constructor
             entity.setPosition(entity.getX(), shooter.getEyeY() - 0.1f, entity.getZ());
             return;
         }
@@ -105,15 +120,25 @@ public class OmniEnchantment extends Enchantment {
             return;
         }
 
-        entity.setVelocity(getProjectileVel(shooter, 1f));
+        if (entity instanceof FallingBlockEntity fallingBlockEntity && fallingBlockEntity.getBlockState().isIn(BlockTags.ANVIL)) { // TODO: Make this a tag
+            entity.setVelocity(getProjectileVel(shooter, 1f));
+            return;
+        }
+
+        entity.setYaw(shooter.getYaw());
+        entity.setVelocity(getProjectileVel(shooter, 1.5f));
     }
 
-    public static boolean shootProjectile(World world, LivingEntity shooter, ItemStack projectile) {
+    public static boolean shootProjectile(ServerWorld world, LivingEntity shooter, ItemStack projectile) {
         var entity = createProjectile(world, shooter, projectile);
         if (entity == null) return false;
-        setupProjectile(entity, world, shooter, projectile);
+        setupProjectile(entity, shooter, projectile);
         world.spawnEntity(entity);
         return true;
     }
 
+    public static ItemStack getRemainder(ItemStack projectile) {
+        if (projectile.getItem() instanceof EntityBucketItem) return Items.BUCKET.getDefaultStack();
+        return projectile.getRecipeRemainder();
+    }
 }
--- src/main/java/archives/tater/omnicrossbow/mixin/CrossbowItemMixin.java
+++ src/main/java/archives/tater/omnicrossbow/mixin/CrossbowItemMixin.java
@@ -2,8 +2,10 @@ package archives.tater.omnicrossbow.mixin;
 
 import archives.tater.omnicrossbow.OmniEnchantment;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
@@ -12,16 +14,38 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CrossbowItem.class)
-public class CrossbowItemMixin {
+public abstract class CrossbowItemMixin {
+    @Inject(
+            method = "loadProjectile",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;split(I)Lnet/minecraft/item/ItemStack;")
+    )
+    private static void giveRemainder(LivingEntity shooter, ItemStack crossbow, ItemStack projectile, boolean simulated, boolean creative, CallbackInfoReturnable<Boolean> cir) {
+        var remainder = OmniEnchantment.getRemainder(projectile);
+        if (remainder.isEmpty()) return;
+        if (projectile.getCount() <= 1) {
+            if (shooter.getStackInHand(Hand.MAIN_HAND) == projectile) {
+                shooter.setStackInHand(Hand.MAIN_HAND, remainder);
+                return;
+            } else if (shooter.getStackInHand(Hand.OFF_HAND) == projectile) {
+                shooter.setStackInHand(Hand.OFF_HAND, remainder);
+                return;
+            }
+        }
+        if (shooter instanceof PlayerEntity playerEntity)
+            if (playerEntity.giveItemStack(remainder)) return;
+        shooter.dropStack(remainder);
+    }
+
     @Inject(
             method = "shoot",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/item/CrossbowItem;createArrow(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/item/ItemStack;)Lnet/minecraft/entity/projectile/PersistentProjectileEntity;"),
             cancellable = true
     )
     private static void customProjectile(World world, LivingEntity shooter, Hand hand, ItemStack crossbow, ItemStack projectile, float soundPitch, boolean creative, float speed, float divergence, float simulated, CallbackInfo ci) {
-        if (!OmniEnchantment.shootProjectile(world, shooter, projectile)) return;
+        if (!OmniEnchantment.shootProjectile((ServerWorld) world, shooter, projectile)) return;
         ci.cancel();
         crossbow.damage(1, shooter, e -> e.sendToolBreakStatus(hand));
         world.playSound(null, shooter.getX(), shooter.getY(), shooter.getZ(), SoundEvents.ITEM_CROSSBOW_SHOOT, SoundCategory.PLAYERS, 1.0F, soundPitch);
