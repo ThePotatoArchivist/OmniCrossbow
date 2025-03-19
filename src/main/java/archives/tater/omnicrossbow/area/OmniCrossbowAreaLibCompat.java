6a593b6b73220bebaccac59beb4adc71595a839f
ThePotatoArchivist <josiahfu@gmail.com>
2025-03-19 13:15:40 -0700

    Require operator level for area command

--- src/main/java/archives/tater/omnicrossbow/area/OmniCrossbowAreaLibCompat.java
+++ src/main/java/archives/tater/omnicrossbow/area/OmniCrossbowAreaLibCompat.java
@@ -38,6 +38,7 @@ public class OmniCrossbowAreaLibCompat {
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(literal("omnicrossbow_modifiable_area")
+                    .requires(source -> source.hasPermissionLevel(3))
                     .then(argument(AREA_ARG, AreaArgument.area())
                         .then(argument(ALLOWED_ARG, BoolArgumentType.bool())
                             .executes(command -> runAreaCommand(
