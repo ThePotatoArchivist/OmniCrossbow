8e19fa322b868b65c6c1df3c70a7b47a7c1730e9
JosiahFu <josiahfu@gmail.com>
2025-08-02 19:03:46 -0700

    Switch from overlay back to fog

--- src/client/java/archives/tater/omnicrossbow/mixin/client/endereye/BackgroundRendererMixin.java
+++ src/client/java/archives/tater/omnicrossbow/mixin/client/endereye/BackgroundRendererMixin.java
@@ -4,13 +4,39 @@ import archives.tater.omnicrossbow.entity.SpyEnderEyeEntity;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.world.ClientWorld;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BackgroundRenderer.class)
 public class BackgroundRendererMixin {
+    @Shadow private static float green;
+
+    @Shadow private static float blue;
+
+    @Shadow private static float red;
+
+    @Inject(
+            method = "applyFog",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/BackgroundRenderer$FogData;fogStart:F", opcode = Opcodes.GETFIELD)
+    )
+    private static void fogIfEnderEyeInBlock(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci, @Local BackgroundRenderer.FogData fogData) {
+        if (!(camera.getFocusedEntity() instanceof SpyEnderEyeEntity) || !camera.getFocusedEntity().isInsideWall()) return;
+
+        if (fogData.fogType == BackgroundRenderer.FogType.FOG_SKY) {
+            fogData.fogStart = 0.0F;
+            fogData.fogEnd = 5f * 0.8F;
+        } else {
+            fogData.fogStart = 5f * 0.25F;
+            fogData.fogEnd = 5f;
+        }
+    }
 
     @ModifyVariable(
             method = "render",
@@ -22,5 +48,17 @@ public class BackgroundRendererMixin {
     )
     private static float enderEyeNightVision(float original, @Local(argsOnly = true) Camera camera) {
         return camera.getFocusedEntity() instanceof SpyEnderEyeEntity && !camera.getFocusedEntity().isInsideWall() ? 1f : original;
+    }
+
+    @Inject(
+            method = "render",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BackgroundRenderer;getFogModifier(Lnet/minecraft/entity/Entity;F)Lnet/minecraft/client/render/BackgroundRenderer$StatusEffectFogModifier;")
+    )
+    private static void modifyFogColor(Camera camera, float tickDelta, ClientWorld world, int viewDistance, float skyDarkness, CallbackInfo ci) {
+        if (camera.getFocusedEntity() instanceof SpyEnderEyeEntity && camera.getFocusedEntity().isInsideWall()) {
+            red = 0f;
+            green = 0f;
+            blue = 0f;
+        }
     }
 }
