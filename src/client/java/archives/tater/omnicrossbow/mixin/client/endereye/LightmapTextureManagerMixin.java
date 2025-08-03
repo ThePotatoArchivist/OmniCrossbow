e88ecdd31b380dabba26db32b30c1d0d222281ea
JosiahFu <josiahfu@gmail.com>
2025-08-02 19:54:35 -0700

    Fix night vision sinytra

--- src/client/java/archives/tater/omnicrossbow/mixin/client/endereye/LightmapTextureManagerMixin.java
+++ src/client/java/archives/tater/omnicrossbow/mixin/client/endereye/LightmapTextureManagerMixin.java
@@ -1,26 +1,23 @@
 package archives.tater.omnicrossbow.mixin.client.endereye;
 
 import archives.tater.omnicrossbow.entity.SpyEnderEyeEntity;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(LightmapTextureManager.class)
 public class LightmapTextureManagerMixin {
     @Shadow @Final private MinecraftClient client;
 
-    @ModifyVariable(
+    @ModifyExpressionValue(
             method = "update",
-            at = @At(value = "INVOKE", target = "Lorg/joml/Vector3f;<init>(FFF)V", ordinal = 0, remap = false),
-            ordinal = 6
+            at = @At(value = "CONSTANT", args = "floatValue=0.0", ordinal = 1)
     )
     private float nightVisionEnderEye(float value) {
         return value == 0.0 && client.cameraEntity instanceof SpyEnderEyeEntity ? 1 : value;
     }
-
-
 }
