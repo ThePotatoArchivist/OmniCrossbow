9225a667884057d186d1a1c142a046aa9ae7559f
ThePotatoArchivist <josiahfu@gmail.com>
2026-02-07 03:16:30 -0800

    key fix

--- src/main/kotlin/archives/tater/omnicrossbow/condition/CanPickUpLoot.kt
+++ src/main/kotlin/archives/tater/omnicrossbow/condition/CanPickUpLoot.kt
@@ -2,6 +2,7 @@ package archives.tater.omnicrossbow.condition
 
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.util.context.ContextKey
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.level.storage.loot.LootContext
 import net.minecraft.world.level.storage.loot.LootContext.EntityTarget
@@ -12,6 +13,8 @@ data class CanPickUpLoot(val entity: EntityTarget) : LootItemCondition, LootItem
     override fun codec(): MapCodec<out LootItemCondition> = CODEC
 
     override fun test(context: LootContext): Boolean = (entity[context] as? LivingEntity)?.canPickUpLoot() == true
+
+    override fun getReferencedContextParams(): Set<ContextKey<*>> = setOf(entity.contextParam())
 
     override fun build(): LootItemCondition = this
 
