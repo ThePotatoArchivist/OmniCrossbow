d535c72867d737bc1f4c19c938259d31e2ce5ddf
ThePotatoArchivist <josiahfu@gmail.com>
2026-02-08 03:44:30 -0800

    ItemFiltered Specificity sorting

--- src/main/kotlin/archives/tater/omnicrossbow/projectilebehavior/ItemFiltered.kt
+++ src/main/kotlin/archives/tater/omnicrossbow/projectilebehavior/ItemFiltered.kt
@@ -6,9 +6,11 @@ import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.advancements.criterion.ItemPredicate
 import net.minecraft.core.HolderLookup
+import net.minecraft.core.HolderSet
 import net.minecraft.core.Registry
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.item.ItemStack
+import java.util.Comparator.comparingInt
 import java.util.stream.Stream
 import kotlin.jvm.optionals.getOrNull
 
@@ -16,7 +18,11 @@ import kotlin.jvm.optionals.getOrNull
 data class ItemFiltered<T>(
     val items: ItemPredicate,
     val value: T
-) {
+) : Comparable<ItemFiltered<T>> {
+
+    override fun compareTo(other: ItemFiltered<T>): Int =
+        ITEM_PREDICATE_COMPARATOR.compare(this.items, other.items)
+
     companion object {
         fun <T> createCodec(valueCodec: MapCodec<T>): Codec<ItemFiltered<T>> = RecordCodecBuilder.create { it.group(
             ITEM_PREDICATE_SHORT_CODEC.fieldOf("items").forGetter(ItemFiltered<T>::items),
@@ -28,21 +34,24 @@ data class ItemFiltered<T>(
             valueCodec.fieldOf("value").forGetter(ItemFiltered<T>::value)
         ).apply(it, ::ItemFiltered) }
 
-        fun <T: Any> getFirst(registries: HolderLookup.Provider, registry: ResourceKey<Registry<ItemFiltered<T>>>, key: ItemStack): T? = registries
-            .lookupOrThrow(registry)
-            .listElements()
-            .map { it.value() }
-            .filter { it.items.test(key) }
-            .findFirst()
-            .map { it.value }
-            .getOrNull()
+        val ITEM_PREDICATE_COMPARATOR: Comparator<ItemPredicate> =
+            comparingInt<ItemPredicate> { predicate -> predicate.items.getOrNull()?.takeIf { it is HolderSet.Direct }?.size() ?: Int.MAX_VALUE }
+            .thenComparingInt { if (it.items.isPresent) 0 else 1 }
+            .thenComparingInt { if (it.components.exact.isEmpty) 1 else 0 }
+            .then(comparingInt<ItemPredicate> { it.components.partial.size }.reversed())
 
         fun <T: Any> streamMatching(registries: HolderLookup.Provider, registry: ResourceKey<Registry<ItemFiltered<T>>>, key: ItemStack): Stream<T> = registries
             .lookupOrThrow(registry)
             .listElements()
             .map { it.value() }
+            .sorted()
             .filter { it.items.test(key) }
             .map { it.value }
+
+        fun <T: Any> getFirst(registries: HolderLookup.Provider, registry: ResourceKey<Registry<ItemFiltered<T>>>, key: ItemStack): T? =
+            streamMatching(registries, registry, key)
+            .findFirst()
+            .getOrNull()
     }
 }
 
