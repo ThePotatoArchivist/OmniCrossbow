1c38ee63faa13af001bdf906838c018111118f0b
ThePotatoArchivist <josiahfu@gmail.com>
2026-02-15 01:41:34 -0800

    Fix fake player name

--- src/main/kotlin/archives/tater/omnicrossbow/entity/FakePlayerUtil.kt
+++ src/main/kotlin/archives/tater/omnicrossbow/entity/FakePlayerUtil.kt
@@ -2,12 +2,10 @@ package archives.tater.omnicrossbow.entity
 
 import archives.tater.omnicrossbow.mixin.behavior.access.EntityAccessor
 import archives.tater.omnicrossbow.mixin.behavior.access.LivingEntityInvoker
-import archives.tater.omnicrossbow.util.ifNotNull
-import archives.tater.omnicrossbow.util.orElse
 import net.fabricmc.fabric.api.entity.FakePlayer
+import com.mojang.authlib.GameProfile
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
-import net.minecraft.world.entity.player.Player
 import net.minecraft.world.phys.Vec3
 
 fun createFakePlayer(
@@ -16,11 +14,11 @@ fun createFakePlayer(
     pos: Vec3 = projectile.position(),
     xRot: Float = projectile.xRot,
     yRot: Float = projectile.yRot,
-): FakePlayer = (ifNotNull(projectile.owner as? Player) {
-    FakePlayer.get(level, it.gameProfile) // TODO chamge username
-} orElse {
-    FakePlayer.get(level)
-}).apply {
+): FakePlayer = FakePlayer.get(
+    level,
+    projectile.owner?.let { GameProfile(it.uuid, "${projectile.item.hoverName.string} shot by ${it.displayName?.string}") }
+        ?: GameProfile(FakePlayer.DEFAULT_UUID, "flying ${projectile.item.hoverName.string}")
+).apply {
     this as EntityAccessor
     this as LivingEntityInvoker
     snapTo(pos, yRot, xRot)
