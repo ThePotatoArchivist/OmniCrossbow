768fdd59bef8f1db22a9d543a4949315666b3381
ThePotatoArchivist <josiahfu@gmail.com>
2026-02-18 18:49:12 -0800

    Simplify damage code due to fake player damage handling

--- src/main/kotlin/archives/tater/omnicrossbow/projectilebehavior/impactaction/Damage.kt
+++ src/main/kotlin/archives/tater/omnicrossbow/projectilebehavior/impactaction/Damage.kt
@@ -2,17 +2,13 @@ package archives.tater.omnicrossbow.projectilebehavior.impactaction
 
 import archives.tater.omnicrossbow.entity.CustomItemProjectile
 import archives.tater.omnicrossbow.entity.DelegateFakePlayer
-import archives.tater.omnicrossbow.mixin.behavior.access.LivingEntityInvoker
-import archives.tater.omnicrossbow.mixin.behavior.access.PlayerInvoker
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.util.ExtraCodecs
-import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.enchantment.EnchantmentHelper
 import net.minecraft.world.phys.EntityHitResult
 import net.minecraft.world.phys.HitResult
 
@@ -33,34 +29,11 @@ data class Damage(val baseAttackDamage: Float = 1f) : ImpactAction.Inline {
         val entity = (hit as? EntityHitResult)?.entity ?: return false
 
         val fakePlayer = DelegateFakePlayer.create(level, projectile)
-        fakePlayer as PlayerInvoker
-        fakePlayer as LivingEntityInvoker
 
-        val owner = projectile.owner as? LivingEntity ?: fakePlayer
         val instance = fakePlayer.getAttribute(Attributes.ATTACK_DAMAGE) ?: return false
         instance.baseValue = baseAttackDamage.toDouble()
 
-        val baseDamage = instance.value.toFloat()
-        val attackingItemStack = projectile.item
-        val damageSource = attackingItemStack.getDamageSource(owner) { level.damageSources().run { if (owner is Player) playerAttack(owner) else mobAttack(owner) } }
-        val magicBoost = EnchantmentHelper.modifyDamage(level, attackingItemStack, entity, damageSource, baseDamage) - baseDamage
-
-        if (baseDamage <= 0f && magicBoost <= 0f) return false
-
-        val totalDamage = baseDamage + attackingItemStack.item.getAttackDamageBonus(entity, baseDamage, damageSource) + magicBoost
-
-        val oldMovement = entity.deltaMovement
-        if (!entity.hurtServer(level, damageSource, totalDamage)) return false
-
-        fakePlayer.causeExtraKnockback(
-            entity,
-            fakePlayer.invokeGetKnockback(entity, damageSource),
-            oldMovement
-        )
-
-        (owner as? PlayerInvoker ?: fakePlayer).invokeItemAttackInteraction(entity, attackingItemStack, damageSource, true)
-
-//        fakePlayer.postPiercingAttack()
+        fakePlayer.attack(entity)
 
         return true
     }
--- src/main/kotlin/archives/tater/omnicrossbow/projectilebehavior/impactaction/KineticDamage.kt
+++ src/main/kotlin/archives/tater/omnicrossbow/projectilebehavior/impactaction/KineticDamage.kt
@@ -10,12 +10,12 @@ import net.minecraft.core.component.DataComponents
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.util.ExtraCodecs
 import net.minecraft.util.Mth.floor
+import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.ai.attributes.Attributes
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.component.KineticWeapon.getMotion
-import net.minecraft.world.item.enchantment.EnchantmentHelper
 import net.minecraft.world.phys.EntityHitResult
 import net.minecraft.world.phys.HitResult
 
@@ -62,28 +62,7 @@ data class KineticDamage(val baseAttackDamage: Float = 1f) : ImpactAction.Inline
 
         val damageDealt = instance.value.toFloat() + floor(relativeSpeed * kineticWeapon.damageMultiplier)
 
-        val damageSource = attackingItemStack.getDamageSource(owner) { level.damageSources().run { if (owner is Player) playerAttack(owner) else mobAttack(owner) } }
-        val magicBoost = EnchantmentHelper.modifyDamage(level, attackingItemStack, entity, damageSource, damageDealt) - damageDealt
-
-        val totalDamage = if (dealsDamage) damageDealt + magicBoost else 0.0f
-
-        val oldMovement = entity.deltaMovement
-        val wasHurt = dealsDamage && entity.hurtServer(level, damageSource, totalDamage)
-        if (dealsKnockback) {
-            fakePlayer.causeExtraKnockback(entity, 0.4f + fakePlayer.invokeGetKnockback(entity, damageSource), oldMovement)
-        }
-
-        var dismounted = false
-        if (dealsDismount && entity.isPassenger) {
-            dismounted = true
-            entity.stopRiding()
-        }
-
-        if (!wasHurt && !dealsKnockback && !dismounted) return false
-
-        (owner as? PlayerInvoker ?: fakePlayer).invokeItemAttackInteraction(entity, attackingItemStack, damageSource, wasHurt)
-
-        return true
+        return fakePlayer.stabAttack(EquipmentSlot.MAINHAND, entity, damageDealt, dealsDamage, dealsKnockback, dealsDismount)
     }
 
     override val codec: MapCodec<out KineticDamage> get() = CODEC
