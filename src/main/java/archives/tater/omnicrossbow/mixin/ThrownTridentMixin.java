1d7cb116d79c37fa358ae25f85e4fdff0f54286f
ThePotatoArchivist <josiahfu@gmail.com>
2026-05-05 16:40:58 -0700

    Fix trident foil & knife

--- src/main/java/archives/tater/omnicrossbow/mixin/ThrownTridentMixin.java
+++ src/main/java/archives/tater/omnicrossbow/mixin/ThrownTridentMixin.java
@@ -2,18 +2,28 @@ package archives.tater.omnicrossbow.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.projectile.arrow.AbstractArrow;
 import net.minecraft.world.entity.projectile.arrow.ThrownTrident;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.ValueInput;
 
 @Mixin(ThrownTrident.class)
 public abstract class ThrownTridentMixin extends AbstractArrow {
+    @Shadow
+    @Final
+    private static EntityDataAccessor<Boolean> ID_FOIL;
+
     protected ThrownTridentMixin(EntityType<? extends AbstractArrow> type, Level level) {
         super(type, level);
     }
@@ -24,5 +34,13 @@ public abstract class ThrownTridentMixin extends AbstractArrow {
     )
     private boolean preventPickup(Inventory instance, ItemStack itemStack, Operation<Boolean> original) {
         return pickup == Pickup.CREATIVE_ONLY || original.call(instance, itemStack);
+    }
+
+    @Inject(
+            method = "readAdditionalSaveData",
+            at = @At("TAIL")
+    )
+    private void fixFoil(ValueInput input, CallbackInfo ci) {
+        entityData.set(ID_FOIL, getPickupItem().hasFoil());
     }
 }
